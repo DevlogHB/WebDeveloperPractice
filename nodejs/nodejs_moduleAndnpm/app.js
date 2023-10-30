5af8254b378f0bb148102ffff9e56908c1bdfdcf
HongBin <itshb1221@gmail.com>
2023-10-30 17:01:42 +0900

    create nodejs modules&npm ex

---
+++ nodejs/nodejs_moduleAndnpm/app.js
@@ -0,0 +1,9 @@
+// const math = require('./math');
+const {PI, square} = require('./math');
+
+// console.log(math);
+console.log(PI);
+console.log(square(9));
+
+const cats = require('./shelter');
+console.log('required an entire directory!' + cats);
